9b8a2907d3e5b4dfd8fd21653c9f3c670734dd73
Daffa Randika <daffa.randika.3@gmail.com>
2024-09-13 17:28:18 +0700

    fungsi dasar dari program

---
+++ include/my_header.hpp
@@ -0,0 +1,9 @@
+#ifndef MY_HEADER_HPP
+#define MY_HEADER_HPP
+#include <iostream>
+
+void greet() {
+    std::cout << "Hello from CMake!" << std::endl;
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "my_header.hpp"
+
+int main() {
+    greet();
+    return 0;
+}
+
+
+
